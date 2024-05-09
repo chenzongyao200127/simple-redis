53fc9c83da8df350c23c2061c7473bea58491cd0
chenzongyao200127 <2319251792@qq.com>
2024-05-09 21:41:05 +0800

    feature: support RespFrame decode

--- src/resp/encode.rs
+++ src/resp/encode.rs
@@ -1,21 +1,3 @@
-/*
-    如何解析 Frame
-    - simple string: "+OK\r\n"
-    - error: "-ERROR message\r\n"
-    - bulk error: "!<length>\r\n<error>\r\n"
-    - integer: ":[<+|->]<value>\r\n"
-    - bulk string: "$<length>\r\n<data>\r\n"
-    - null bulk string: "$-1\r\n"
-    - array: "*<number-of-elements>\r\n<element-1>...<element-n>"
-    - null array: "*-1\r\n"
-    - null: "_\r\n"
-    - boolean: "#<t|f>\r\n"
-    - double: ",[<+|->]<integral>[.<fractional>][<E|e>[sign]<exponent>]\r\n"
-    - big number: "b<length>\r\n<data>\r\n"
-    - map: "%<number-of-elements>\r\n<key-1><value-1>...<key-n><value-n>"
-    - set: "~<number-of-elements>\r\n<element-1>...<element-n>"
-*/
-
 use crate::{
     BulkString, RespArray, RespEncode, RespMap, RespNull, RespNullArray, RespNullBulkString,
     RespSet, SimpleError, SimpleString,
@@ -23,57 +5,51 @@ use crate::{
 
 const BUF_CAP: usize = 4096;
 
-// simple string: "+OK\r\n"
+// - simple string: "+OK\r\n"
 impl RespEncode for SimpleString {
     fn encode(self) -> Vec<u8> {
         format!("+{}\r\n", self.0).into_bytes()
     }
 }
 
-// error: "-ERROR message\r\n"
+// - error: "-Error message\r\n"
 impl RespEncode for SimpleError {
     fn encode(self) -> Vec<u8> {
         format!("-{}\r\n", self.0).into_bytes()
     }
 }
 
-// bulk string: "$<length>\r\n<data>\r\n"
+// - integer: ":[<+|->]<value>\r\n"
+impl RespEncode for i64 {
+    fn encode(self) -> Vec<u8> {
+        let sign = if self < 0 { "" } else { "+" };
+        format!(":{}{}\r\n", sign, self).into_bytes()
+    }
+}
+
+// - bulk string: "$<length>\r\n<data>\r\n"
 impl RespEncode for BulkString {
     fn encode(self) -> Vec<u8> {
         let mut buf = Vec::with_capacity(self.len() + 16);
-        buf.extend_from_slice(format!("${}\r\n", self.len()).as_bytes());
+        buf.extend_from_slice(&format!("${}\r\n", self.len()).into_bytes());
         buf.extend_from_slice(&self);
         buf.extend_from_slice(b"\r\n");
         buf
     }
 }
 
-// integer: ":[<+|->]<value>\r\n"
-impl RespEncode for i64 {
-    fn encode(self) -> Vec<u8> {
-        let mut buf = Vec::with_capacity(32);
-        let ret = if self < 0 {
-            format!(":{}\r\n", self)
-        } else {
-            format!(":+{}\r\n", self)
-        };
-        buf.extend_from_slice(&ret.into_bytes());
-        buf
-    }
-}
-
-// null bulk string: "$-1\r\n"
+// - null bulk string: "$-1\r\n"
 impl RespEncode for RespNullBulkString {
     fn encode(self) -> Vec<u8> {
         b"$-1\r\n".to_vec()
     }
 }
 
-// array: "*<number-of-elements>\r\n<element-1>...<element-n>"
+// - array: "*<number-of-elements>\r\n<element-1>...<element-n>"
 impl RespEncode for RespArray {
     fn encode(self) -> Vec<u8> {
         let mut buf = Vec::with_capacity(BUF_CAP);
-        buf.extend_from_slice(format!("*{}\r\n", self.len()).as_bytes());
+        buf.extend_from_slice(&format!("*{}\r\n", self.0.len()).into_bytes());
         for frame in self.0 {
             buf.extend_from_slice(&frame.encode());
         }
@@ -81,49 +57,49 @@ impl RespEncode for RespArray {
     }
 }
 
-// null array: "*-1\r\n"
+// - null array: "*-1\r\n"
 impl RespEncode for RespNullArray {
     fn encode(self) -> Vec<u8> {
         b"*-1\r\n".to_vec()
     }
 }
 
-// null: "_\r\n"
+// - null: "_\r\n"
 impl RespEncode for RespNull {
     fn encode(self) -> Vec<u8> {
         b"_\r\n".to_vec()
     }
 }
 
-// boolean: "#<t|f>\r\n"
+// - boolean: "#<t|f>\r\n"
 impl RespEncode for bool {
     fn encode(self) -> Vec<u8> {
         format!("#{}\r\n", if self { "t" } else { "f" }).into_bytes()
     }
 }
 
-// double: ",[<+|->]<integral>[.<fractional>][<E|e>[sign]<exponent>]\r\n"
+// - double: ",[<+|->]<integral>[.<fractional>][<E|e>[sign]<exponent>]\r\n"
 impl RespEncode for f64 {
     fn encode(self) -> Vec<u8> {
         let mut buf = Vec::with_capacity(32);
-
         let ret = if self.abs() > 1e+8 || self.abs() < 1e-8 {
             format!(",{:+e}\r\n", self)
         } else {
             let sign = if self < 0.0 { "" } else { "+" };
             format!(",{}{}\r\n", sign, self)
         };
+
         buf.extend_from_slice(&ret.into_bytes());
         buf
     }
 }
 
-// map: "%<number-of-elements>\r\n<key-1><value-1>...<key-n><value-n>"
-// We only support string keys which are converted to SimpleString
+// - map: "%<number-of-entries>\r\n<key-1><value-1>...<key-n><value-n>"
+// we only support string key which encode to SimpleString
 impl RespEncode for RespMap {
     fn encode(self) -> Vec<u8> {
         let mut buf = Vec::with_capacity(BUF_CAP);
-        buf.extend_from_slice(format!("%{}\r\n", self.len()).as_bytes());
+        buf.extend_from_slice(&format!("%{}\r\n", self.len()).into_bytes());
         for (key, value) in self.0 {
             buf.extend_from_slice(&SimpleString::new(key).encode());
             buf.extend_from_slice(&value.encode());
@@ -132,11 +108,11 @@ impl RespEncode for RespMap {
     }
 }
 
-// set: "~<number-of-elements>\r\n<element-1>...<element-n>"
+// - set: "~<number-of-elements>\r\n<element-1>...<element-n>"
 impl RespEncode for RespSet {
     fn encode(self) -> Vec<u8> {
         let mut buf = Vec::with_capacity(BUF_CAP);
-        buf.extend_from_slice(format!("~{}\r\n", self.len()).as_bytes());
+        buf.extend_from_slice(&format!("~{}\r\n", self.len()).into_bytes());
         for frame in self.0 {
             buf.extend_from_slice(&frame.encode());
         }
@@ -146,7 +122,6 @@ impl RespEncode for RespSet {
 
 #[cfg(test)]
 mod tests {
-
     use crate::RespFrame;
 
     use super::*;
@@ -154,13 +129,24 @@ mod tests {
     #[test]
     fn test_simple_string_encode() {
         let frame: RespFrame = SimpleString::new("OK".to_string()).into();
+
         assert_eq!(frame.encode(), b"+OK\r\n");
     }
 
     #[test]
     fn test_error_encode() {
-        let frame: RespFrame = SimpleError::new("ERROR message".to_string()).into();
-        assert_eq!(frame.encode(), b"-ERROR message\r\n");
+        let frame: RespFrame = SimpleError::new("Error message".to_string()).into();
+
+        assert_eq!(frame.encode(), b"-Error message\r\n");
+    }
+
+    #[test]
+    fn test_integer_encode() {
+        let frame: RespFrame = 123.into();
+        assert_eq!(frame.encode(), b":+123\r\n");
+
+        let frame: RespFrame = (-123).into();
+        assert_eq!(frame.encode(), b":-123\r\n");
     }
 
     #[test]
@@ -178,14 +164,14 @@ mod tests {
     #[test]
     fn test_array_encode() {
         let frame: RespFrame = RespArray::new(vec![
-            BulkString::new("OK".to_string()).into(),
-            BulkString::new(b"hello".to_vec()).into(),
-            123.into(),
+            BulkString::new("set".to_string()).into(),
+            BulkString::new("hello".to_string()).into(),
+            BulkString::new("world".to_string()).into(),
         ])
         .into();
         assert_eq!(
-            String::from_utf8_lossy(&frame.encode()),
-            "*3\r\n$2\r\nOK\r\n$5\r\nhello\r\n:+123\r\n"
+            &frame.encode(),
+            b"*3\r\n$3\r\nset\r\n$5\r\nhello\r\n$5\r\nworld\r\n"
         );
     }
 
@@ -211,35 +197,18 @@ mod tests {
     }
 
     #[test]
-    fn test_integer_encode() {
-        let frame: RespFrame = 123.into();
-        // assert_eq!(String::from_utf8_lossy(&frame.encode()), ":+123\r\n");
-        assert_eq!(frame.encode(), b":+123\r\n");
-
-        let frame: RespFrame = (-123).into();
-        // assert_eq!(String::from_utf8_lossy(&frame.encode()), ":-123\r\n");
-        assert_eq!(frame.encode(), b":-123\r\n");
-    }
-
-    #[test]
     fn test_double_encode() {
-        let frame: RespFrame = 3.15.into();
-        assert_eq!(frame.encode(), b",+3.15\r\n");
+        let frame: RespFrame = 123.456.into();
+        assert_eq!(frame.encode(), b",+123.456\r\n");
 
-        let frame: RespFrame = (-3.15).into();
-        assert_eq!(frame.encode(), b",-3.15\r\n");
-
-        let frame: RespFrame = 1.23456e+8.into();
-        assert_eq!(String::from_utf8_lossy(&frame.encode()), ",+1.23456e8\r\n");
+        let frame: RespFrame = (-123.456).into();
+        assert_eq!(frame.encode(), b",-123.456\r\n");
 
         let frame: RespFrame = 1.23456e+8.into();
         assert_eq!(frame.encode(), b",+1.23456e8\r\n");
 
         let frame: RespFrame = (-1.23456e-9).into();
-        assert_eq!(String::from_utf8_lossy(&frame.encode()), ",-1.23456e-9\r\n");
-
-        let frame: RespFrame = (-1.23456e-9).into();
-        assert_eq!(frame.encode(), b",-1.23456e-9\r\n");
+        assert_eq!(&frame.encode(), b",-1.23456e-9\r\n");
     }
 
     #[test]
@@ -249,45 +218,25 @@ mod tests {
             "hello".to_string(),
             BulkString::new("world".to_string()).into(),
         );
-        map.insert("foo".to_string(), (-123.456).into());
+        map.insert("foo".to_string(), (-123456.789).into());
 
         let frame: RespFrame = map.into();
         assert_eq!(
-            String::from_utf8_lossy(&frame.encode()),
-            "%2\r\n+foo\r\n,-123.456\r\n+hello\r\n$5\r\nworld\r\n"
+            &frame.encode(),
+            b"%2\r\n+foo\r\n,-123456.789\r\n+hello\r\n$5\r\nworld\r\n"
         );
-
-        let mut map = RespMap::new();
-        map.insert(
-            "hello".to_string(),
-            BulkString::new("world".to_string()).into(),
-        );
-        map.insert("foo".to_string(), (-123.456).into());
-
-        let frame: RespFrame = map.into();
-        assert_eq!(
-            frame.encode(),
-            b"%2\r\n+foo\r\n,-123.456\r\n+hello\r\n$5\r\nworld\r\n"
-        )
     }
 
     #[test]
     fn test_set_encode() {
-        let frame: RespFrame = RespSet::new(vec![
-            RespArray::new(vec![1234.into(), true.into()]).into(),
-            BulkString::new(b"OK".to_vec()).into(),
+        let frame: RespFrame = RespSet::new([
+            RespArray::new([1234.into(), true.into()]).into(),
+            BulkString::new("world".to_string()).into(),
         ])
         .into();
         assert_eq!(
-            String::from_utf8_lossy(&frame.encode()),
-            "~2\r\n*2\r\n:+1234\r\n#t\r\n$2\r\nOK\r\n"
+            frame.encode(),
+            b"~2\r\n*2\r\n:+1234\r\n#t\r\n$5\r\nworld\r\n"
         );
-
-        let frame: RespFrame = RespSet::new(vec![
-            RespArray::new(vec![1234.into(), true.into()]).into(),
-            BulkString::new(b"OK".to_vec()).into(),
-        ])
-        .into();
-        assert_eq!(frame.encode(), b"~2\r\n*2\r\n:+1234\r\n#t\r\n$2\r\nOK\r\n");
     }
 }
