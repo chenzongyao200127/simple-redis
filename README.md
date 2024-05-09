3cd48ebbe103a1dd03ebc074095db51c16e01f04
chenzongyao200127 <2319251792@qq.com>
2024-05-09 19:28:51 +0800

    feature: resp encode

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Geektime Rust Simple-redis
 
 一个简单的 redis server 实现
+
+Task:
+1. 如何解析(serialize) Frame
+2. enum RespFream {}
+3. trait RespEncode / RespDecode (enum dispatch)
+4. bytes trait
---
+++ src/resp/mod.rs
@@ -0,0 +1,163 @@
+use anyhow::Result;
+use bytes::BytesMut;
+use enum_dispatch::enum_dispatch;
+use std::collections::BTreeMap;
+use std::ops::{Deref, DerefMut};
+
+mod decode;
+mod encode;
+
+#[enum_dispatch]
+pub trait RespEncode {
+    fn encode(self) -> Vec<u8>;
+}
+
+pub trait RespDecode {
+    fn decode(buf: Self) -> Result<RespFrame, String>;
+}
+
+impl RespDecode for BytesMut {
+    fn decode(_buf: Self) -> Result<RespFrame, String> {
+        todo!()
+    }
+}
+
+#[enum_dispatch(RespEncode)]
+#[derive(Debug, PartialEq, PartialOrd)]
+pub enum RespFrame {
+    SimpleString(SimpleString),
+    Error(SimpleError),
+    Integer(i64),
+    BulkString(BulkString),
+    NullBulkString(RespNullBulkString),
+    Array(RespArray),
+    NullArray(RespNullArray),
+    Null(RespNull),
+    Boolean(bool),
+    Double(f64),
+    Map(RespMap),
+    Set(RespSet),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct SimpleString(String);
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct SimpleError(String);
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct BulkString(Vec<u8>);
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct RespArray(Vec<RespFrame>);
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct RespNull;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct RespNullArray;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd)]
+pub struct RespNullBulkString;
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct RespMap(BTreeMap<String, RespFrame>);
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct RespSet(Vec<RespFrame>);
+
+impl Deref for SimpleString {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for SimpleError {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for BulkString {
+    type Target = Vec<u8>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespArray {
+    type Target = Vec<RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespMap {
+    type Target = BTreeMap<String, RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Default for RespMap {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl DerefMut for RespMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Deref for RespSet {
+    type Target = Vec<RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl SimpleString {
+    pub fn new(s: impl Into<String>) -> Self {
+        Self(s.into())
+    }
+}
+
+impl SimpleError {
+    pub fn new(s: impl Into<String>) -> Self {
+        Self(s.into())
+    }
+}
+
+impl BulkString {
+    pub fn new(s: impl Into<Vec<u8>>) -> Self {
+        Self(s.into())
+    }
+}
+
+impl RespArray {
+    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
+        Self(s.into())
+    }
+}
+
+impl RespMap {
+    pub fn new() -> Self {
+        RespMap(BTreeMap::new())
+    }
+}
+
+impl RespSet {
+    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
+        Self(s.into())
+    }
+}
